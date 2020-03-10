9fa6a78e17b5a6ac54ece4938d3c181a07df2141
Davide Pesavento <davidepesa@gmail.com>
2020-03-10 19:47:06 -0400

    ci: update Travis configuration
     * Add testing on arm64, ppc64le, and s390x with select compiler versions
     * Drop all versions of gcc < 7 and clang < 5
     * Add clang 11 (development branch)
     * Reenable ASan on Linux with clang 7, the upstream bug has been fixed
     * Drop Xcode 9.2 (macOS 10.12 is no longer supported)
     * Upgrade Xcode 11 to 11.3
     * Workaround issue with missing /usr/local/opt/openssl on macOS
     * Various cleanups
    
    Change-Id: Ifc7224cf3e79c74225efefc278bcb547ac862698

--- .jenkins.d/10-build.sh
+++ .jenkins.d/10-build.sh
@@ -16,26 +16,26 @@ fi
 sudo_preserve_env PATH -- ./waf --color=yes distclean
 
 if [[ $JOB_NAME != *"code-coverage" && $JOB_NAME != *"limited-build" ]]; then
-  # Configure/build in optimized mode with tests
-  ./waf --color=yes configure --with-tests
-  ./waf --color=yes build -j${WAF_JOBS:-1}
+    # Build in release mode with tests
+    ./waf --color=yes configure --with-tests
+    ./waf --color=yes build -j${WAF_JOBS:-1}
 
-  # Cleanup
-  sudo_preserve_env PATH -- ./waf --color=yes distclean
+    # Cleanup
+    sudo_preserve_env PATH -- ./waf --color=yes distclean
 
-  # Configure/build in optimized mode without tests
-  ./waf --color=yes configure
-  ./waf --color=yes build -j${WAF_JOBS:-1}
+    # Build in release mode without tests
+    ./waf --color=yes configure
+    ./waf --color=yes build -j${WAF_JOBS:-1}
 
-  # Cleanup
-  sudo_preserve_env PATH -- ./waf --color=yes distclean
+    # Cleanup
+    sudo_preserve_env PATH -- ./waf --color=yes distclean
 fi
 
-# Configure/build in debug mode with tests
+# Build in debug mode with tests
 ./waf --color=yes configure --debug --with-tests $ASAN $COVERAGE
 ./waf --color=yes build -j${WAF_JOBS:-1}
 
-# (tests will be run against debug version)
+# (tests will be run against the debug version)
 
 # Install
 sudo_preserve_env PATH -- ./waf --color=yes install
