e8c82ef8a111549a713c53440310479d502f2099
Davide Pesavento <davidepesa@gmail.com>
2021-07-28 02:38:51 -0400

    ci: add GitHub Actions
    Change-Id: Ic7ed7356fef4af465843b44a5cefd0458c78774d

--- .jenkins.d/00-deps.sh
+++ .jenkins.d/00-deps.sh
@@ -7,9 +7,9 @@ if has OSX $NODE_LABELS; then
         FORMULAE+=(python)
     fi
 
-    if [[ -n $TRAVIS ]]; then
-        # Travis images come with a large number of pre-installed
-        # brew packages, don't waste time upgrading all of them
+    if [[ -n $GITHUB_ACTIONS || -n $TRAVIS ]]; then
+        # GitHub Actions and Travis images come with a large number of
+        # pre-installed packages. Don't waste time upgrading all of them.
         brew list --versions "${FORMULAE[@]}" || brew update
         for FORMULA in "${FORMULAE[@]}"; do
             brew list --versions "$FORMULA" || brew install "$FORMULA"
@@ -23,15 +23,25 @@ if has OSX $NODE_LABELS; then
         brew cleanup
     fi
 
+    if [[ $JOB_NAME == *"Docs" ]]; then
+        pip3 install --upgrade --upgrade-strategy=eager sphinx
+    fi
+
 elif has Ubuntu $NODE_LABELS; then
     sudo apt-get -qq update
     sudo apt-get -qy install build-essential pkg-config python3-minimal \
                              libboost-all-dev libssl-dev libsqlite3-dev \
                              libpcap-dev
 
-    if [[ $JOB_NAME == *"code-coverage" ]]; then
-        sudo apt-get -qy install gcovr lcov
-    fi
+    case $JOB_NAME in
+        *code-coverage)
+            sudo apt-get -qy install gcovr lcov
+            ;;
+        *Docs)
+            sudo apt-get -qy install python3-pip
+            pip3 install --user --upgrade --upgrade-strategy=eager sphinx
+            ;;
+    esac
 
 elif has CentOS-8 $NODE_LABELS; then
     sudo dnf config-manager --enable powertools
