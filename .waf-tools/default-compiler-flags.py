afe3881d2ab75fbcfc6a29f74f72303c2b659bfa
Davide Pesavento <davidepesa@gmail.com>
2026-05-28 22:52:23 -0400

    build: silence warning with clang 22
    Also drop obsolete PPA job
    
    Refs: #5391
    Change-Id: Id537eb4c6d1dae3b1764ab98624a90014fb93022

--- .waf-tools/default-compiler-flags.py
+++ .waf-tools/default-compiler-flags.py
@@ -241,6 +241,8 @@ class ClangFlags(GccClangCommonFlags):
     def getDebugFlags(self, conf):
         flags = super().getDebugFlags(conf)
         flags['CXXFLAGS'] += self.__cxxFlags
+        if get_compiler_ver(conf) >= (22, 0, 0) and get_compiler_ver(conf) < (23, 0, 0):
+            flags['CXXFLAGS'] += ['-Wno-c2y-extensions'] # Bug #5391
         flags['DEFINES'] += [
             # Enable assertions in libc++
             # https://libcxx.llvm.org/Hardening.html
@@ -254,4 +256,6 @@ class ClangFlags(GccClangCommonFlags):
     def getOptimizedFlags(self, conf):
         flags = super().getOptimizedFlags(conf)
         flags['CXXFLAGS'] += self.__cxxFlags
+        if get_compiler_ver(conf) >= (22, 0, 0) and get_compiler_ver(conf) < (23, 0, 0):
+            flags['CXXFLAGS'] += ['-Wno-c2y-extensions'] # Bug #5391
         return flags
